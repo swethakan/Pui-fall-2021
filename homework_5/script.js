ecee3d43fe36608c7b37bc192ef9b7d259e7d67d
Swetha Kannan <swetha@MacBook-Air.hsd1.pa.comcast.net>
2021-10-04 19:11:52 -0400

    more comments

--- homework_5/script.js
+++ homework_5/script.js
@@ -1,3 +1,4 @@
+//Json of all our products
 var products = [
   {
     "product_name": "GPS tracker",
@@ -43,25 +44,33 @@ var products = [
   }
 ];
 
+//Setting up variables that will help the shopping cart functionality
 var itemsInCart = 0;
 var cartNumItems = document.getElementById("cartNumItems");
 var totalPrice = 0;
 var totalPriceDiv = document.getElementById("totalPrice");
+var currentProduct = "";
+var myShoppingCart = document.getElementById("shoppingCart");
+var cartItems = document.getElementById("cartItems");
+var checkoutWarning = document.getElementById("checkoutWarning");
 
+//preferences for each item will be held in these variables
 let color = "Black"; 
 let size = "Tiny"; 
 let quantity = 1;
 let price = 45;
 
-var currentProduct = "";
-var cartItems = document.getElementById("cartItems");
-var checkoutWarning = document.getElementById("checkoutWarning");
-
+//two overlay scenes
 var shoppingOverlay = document.getElementById("shoppingOverlay");
 var confirmOverlay = document.getElementById("confirmationOverly");
 
+//all our buttons for step 1 and for checkout
+var catDogButton = document.getElementById('dogCatButton');
+var catButton = document.getElementById('catButton');
+var dogButton = document.getElementById('dogButton');
 var checkoutButton = document.getElementById("checkoutButton");
 
+//When we load the page, we want the javascript to auto populate the products section based on the json at the top of this file
 var myProductHTML = "<div class = 'allProduct'>"
 for(x = 0; x < products.length; x++){
     myProductHTML += "<div id =product"+x+" tabindex='0' class = 'oneProduct "+products[x].remove_on+"'>"
@@ -76,19 +85,17 @@ for(x = 0; x < products.length; x++){
 myProductHTML += "</div>"
 document.getElementById("products").innerHTML = myProductHTML;
 
-var dogElements = document.getElementsByClassName('dog');
-var catElements = document.getElementsByClassName('cat');
-var catDogButton = document.getElementById('dogCatButton');
-var catButton = document.getElementById('catButton');
-var dogButton = document.getElementById('dogButton');
-
+//Now add an event listener to each product div. This will make them buttons
 var productList = document.getElementsByClassName('oneProduct');
 for(x = 0; x < productList.length; x++){
     productList[x].addEventListener("click", openOverlay);
   }
 
-var myShoppingCart = document.getElementById("shoppingCart");
+//To help our sorting functionality, get all the elements that should leave on dog filter, and ones that should leave on cat filter
+var dogElements = document.getElementsByClassName('dog');
+var catElements = document.getElementsByClassName('cat');
 
+//This handles the visuals of the sorting functionality in step 1
 function turnOffCheck(){
   if(catButton.classList.contains('checked')){
     catButton.classList.remove("checked");
@@ -101,7 +108,7 @@ function turnOffCheck(){
     catDogButton.classList.remove("checked");
   }
 }
-
+//This takes out all products that are dog-only
 function filterOutDog(){
   turnOffCheck();
   catButton.classList.add ("checked");
@@ -112,6 +119,7 @@ function filterOutDog(){
     catElements[x].classList.remove( "remove" );
   }
 }
+//This takes out all products that are cat-only
 function filterOutCat(){
   turnOffCheck();
   dogButton.classList.add ("checked");
@@ -122,6 +130,7 @@ function filterOutCat(){
     catElements[x].classList.add( "remove" );
   }
 }
+//This takes out all products that are for both dogs and cats
 function filterNone(){
   turnOffCheck();
   catDogButton.classList.add ("checked");
@@ -134,6 +143,7 @@ function filterNone(){
   }
 }
 
+//Some more elements that will help us adjust the overlay
 productName = document.getElementById("overlayProductName");
 productPrice = document.getElementById("overlayProductPrice");
 productImage = document.getElementById("overlayProductImage");
@@ -142,8 +152,8 @@ productDescription = document.getElementById("overlayProductDescription");
 confirmName = document.getElementById("confirmProductName");
 confirmImage = document.getElementById("confirmProductImage");
 
+//opens an overlay once user selects a product
 function openOverlay() {
-  console.log(this);
   var productNumber = parseInt( this.id[this.id.length -1] );
   currentProduct = products[productNumber].product_name;
   price = products[productNumber].Price;
@@ -154,18 +164,7 @@ function openOverlay() {
 
   shoppingOverlay.style.display = "block";
 }
-function closeConfirm() {
-  addToCart(currentProduct, color, size, quantity, price);
-  confirmOverlay.style.display = "none";
-  currentProduct = "";
-}
-
-function closeOverlay() {
-  shoppingOverlay.style.display = "none";
-  confirmOverlay.style.display = "block";
-  console.log("visible confirm");
-}
-
+//once you add to cart, this function takes care of the cart and opens a confirmation overlay
 function addToCart(product, color, size, quantity, price) {
   if(itemsInCart == 0){
     checkoutButton.disabled = false;
@@ -182,7 +181,20 @@ function addToCart(product, color, size, quantity, price) {
                         </span> <span class ='color'>Color: <span class='input '><span class = '"+color+"'></span>"+color+"<img alt='dropdown icon' src = 'images/icons/dropdownArrows.svg'></span></span>\
                         <span class ='size'>Size: <span class='input'>"+size+"<img alt='dropdown icon' src = 'images/icons/dropdownArrows.svg'></span></span><span class = 'cartPrice'>Price: <b>$</b><b>"+price+"</b></span></p></div>";
 }
+//closes the overlay that confirms you've added somehting to the cart
+function closeConfirm() {
+  addToCart(currentProduct, color, size, quantity, price);
+  confirmOverlay.style.display = "none";
+  currentProduct = "";
+}
 
+//closes all overlays onscreen
+function closeOverlay() {
+  shoppingOverlay.style.display = "none";
+  confirmOverlay.style.display = "block";
+}
+
+//like it says, when a user wants to get rid of an item, this function deletes it from the cart
 function deleteCartItem(item) {
   //very complicated way to get the price of what was deleted. This would only work if the price is the last element in the description
   let takeawayPrice = item.parentNode.lastChild.lastChild.innerText;
