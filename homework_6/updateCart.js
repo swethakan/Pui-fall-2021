04573e1cd807f2901ca73b6d0f715ca740436395
Swetha <kan_swetha@yahoo.com>
2021-10-24 16:30:24 -0400

    reorganizing files

--- homework_6/updateCart.js
+++
@@ -1,9 +0,0 @@
-let userItems = JSON.parse(localStorage.getItem("shoppingCart"));
-let myCartNumber = document.getElementById("cartNumItems");
-
-function updateCart(){
-  if(userItems!=null){
-    myCartNumber.innerText = userItems.length;
-  }
-}
-document.addEventListener("load", updateCart());
